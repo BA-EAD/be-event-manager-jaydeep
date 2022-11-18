6464fd5d32b5356a047b4c5d88cc88b4d5689b76
jaydeep <jaydeep.c@expertappdevs.dev>
2022-11-18 12:51:46 +0530

    update

--- src/config.ts
+++ src/config.ts
@@ -1,8 +1,8 @@
 export enum appConfig {
-  mongoUrl = 'mongodb://localhost:27017/eventManager',
-  host = 'smtp.gmail.com',
-  service = 'smtp',
-  port = 587,
-  user = 'mtdeshanivanmali@gmail.com',
-  pass = 'nmbjmqpuavooeqlx',
+  mongoUrl = '',
+  host = '',
+  service = '',
+  port = ,
+  user = '',
+  pass = '',
 }
